7b251d5f1bb44627ea3b227a68d0c7a3932cdf3c
Daniel <danielr1609@gmail.com>
2021-08-05 22:03:02 -0400

    Profile and recipe card

---
+++ src/lib/recipeCard.svelte
@@ -0,0 +1,57 @@
+<script>
+    let img = "https://placekitten.com/200/300";
+    let title = "Titulo";
+    //TODO: ponle a la descripcion un maximo de caracteres a mostrar
+    let description = '    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate ut recusandae iusto, quaerat tempore dolorum eligendi? Suscipit corporis tempora eum error, deleniti porro, eaque perspiciatis ducimus, sequi id dignissimos debitis!'
+    let stars = 0;
+</script>
+
+<div class="card">
+    <div class="img">
+        <p>{title}</p>
+        <img src={img} alt="">
+    </div>
+    <p class="descripcion">{description}</p>
+    <p class="stars">{stars}/5</p>
+</div>
+
+<style lang="scss">
+    .card{
+        width: 300px;
+        height: 400px;
+        background: white;
+        box-shadow: 4px 4px 12px rgba($color: #000000, $alpha: .5);
+        margin-bottom: 30px;
+        overflow: hidden;
+        position: relative;
+        .img{
+            height: 200px;
+            overflow: hidden;
+            border-bottom: solid 4px #f05a28;
+            position: relative;
+            p{
+                position: absolute;
+                color: #f05a28;
+                font-weight: bold;
+                padding: 4px 8px;
+                font-size: 1.5em;
+                bottom: 0;
+            }
+            img{
+                object-fit: cover;
+                width: 100%;
+            }
+        }
+        .descripcion{
+            padding: 8px 20px;
+        }
+        .stars{
+            position: absolute;
+            color: #f05a28;
+            font-weight: bold;
+            font-size: 1.2em;
+            bottom: 20px;
+            right: 20px;
+        }
+    }
+</style>
---
+++ src/routes/profile.svelte
@@ -0,0 +1,137 @@
+<script>
+	import RecipeCard from './../lib/recipeCard.svelte';
+    let profileImg = null; //imagen de perfil
+    let username = ''//nombre de usuario
+    let horas = null; //activo hace...
+    const show = () => {
+        document.getElementById("edit").style.visibility = 'visible';
+    }
+    const hide = () => {
+        document.getElementById("edit").style.visibility = 'hidden';
+    }
+</script>
+
+<!--Formulario de registro-->
+<div class="container">
+    <div class="img-bg">
+        <div on:mouseenter={show} on:mouseleave={hide} class="profile-img">
+            <img src="https://placekitten.com/200/300" alt="">
+            <div id="edit"><span class="fas fa-edit"></span></div>
+        </div>
+        <h3>Username</h3>
+        <p>Activo hace: {horas}h</p>
+    </div>
+    <section>
+        <div class="title-container">
+            <h4>Recetas</h4>
+            <div class='linea'></div>
+        </div>
+        <div class="recipes">
+            <RecipeCard/>
+            <RecipeCard/>
+            <RecipeCard/>
+            <RecipeCard/>
+            <RecipeCard/>
+        </div>
+    </section>
+</div>
+
+<style lang="scss">
+.container{
+        display: flex;
+        flex-direction: column;
+        height: calc(100vh - 65px);
+        .img-bg{
+            width: 100%;
+            height: 100%;
+            min-height: 320px;
+            max-height: 450px;
+            overflow: hidden;
+            background-image: url(/img/bg_2.jpg);
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+            border-bottom: solid 2px #f05a28;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            .profile-img{
+                overflow: hidden;
+                width: 200px;
+                height: 200px;
+                border-radius: 50%;
+                display: flex;
+                position: relative;
+                cursor: pointer;
+                img{
+                    width: 100%;
+                    object-fit: cover;
+                }
+                #edit{
+                    position: absolute;
+                    width: 100%;
+                    height: 100%;
+                    background-color: rgba($color: #000000, $alpha: .4);
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                    visibility: hidden;
+                    span{
+                        color: white;
+                        font-size: 1.5em;
+                    }
+                }
+            }
+            h3{
+                padding: 10px 0;
+                font-size: 2em;
+            }
+            @media (min-width: 768px) {
+                max-height: 100%;
+            }
+        }
+        section{
+            background: white;
+            padding: 25px;
+            .title-container{
+                display: flex;
+                align-items: center;
+                margin-bottom: 10px;
+                h4{
+                    font-size: 1.5em;
+                    margin-right: 4px;
+                }
+                .linea{
+                    width: 100%;
+                    border-top: dashed 3px #f05a28;
+                }
+            }
+            .recipes{
+                display: flex;
+                flex-wrap: wrap;
+                justify-content: center;
+                @media (min-width: 768px) {
+                    justify-content: space-between;
+            }
+            }
+            @media (min-width: 768px) {
+                overflow-y: scroll;
+                
+            }
+        }
+    
+    @media (min-width: 768px) {
+        flex-direction: row;
+        .img-bg{
+           border-bottom: none;
+           border-right: solid 2px #f05a28;
+        }
+
+        section{
+            width: 100%;
+        }
+    }
+}
+    
+</style>
