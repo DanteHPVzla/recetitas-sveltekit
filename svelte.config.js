756a61ada32088305f111a68793ed6131744a2c4
Daniel <danielr1609@gmail.com>
2021-08-07 09:30:33 -0400

    firebase json a

--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,7 @@ import firebase from "svelte-adapter-firebase";
 const config = {
 	preprocess: sveltePreprocess(),
 	kit: {
-		adapter: adapter(), // currently the adapter does not take any options
+		adapter: firebase(), // currently the adapter does not take any options
 		
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
