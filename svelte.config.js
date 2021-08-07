9432fad0f9c7f4e531c32c14091ed1c934d56a4d
Daniel <danielr1609@gmail.com>
2021-08-07 09:13:05 -0400

    firebase ready

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,11 @@
 import sveltePreprocess from 'svelte-preprocess'
 import adapter from '@sveltejs/adapter-netlify';
+import firebase from "svelte-adapter-firebase";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: sveltePreprocess(),
 	kit: {
-		adapter: adapter(), // currently the adapter does not take any options
+		adapter: firebase(),
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	}
